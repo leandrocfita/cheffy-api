082ee3bcdd1ad857be3c6c8842af1fca562719a2
Victor Reis <victor.sreis@hotmail.com>
2026-04-26 07:25:57 -0300

    fix:Valida disponibilidade do item ao criar pedido

--- src/main/java/br/com/fiap/cheffy/application/fooditem/service/FoodItemServiceHelper.java
+++ src/main/java/br/com/fiap/cheffy/application/fooditem/service/FoodItemServiceHelper.java
@@ -2,6 +2,7 @@ package br.com.fiap.cheffy.application.fooditem.service;
 
 import br.com.fiap.cheffy.domain.fooditem.entity.FoodItem;
 import br.com.fiap.cheffy.domain.fooditem.exception.FoodItemNotFoundException;
+import br.com.fiap.cheffy.domain.fooditem.exception.FoodItemUnavailableForOrderException;
 import br.com.fiap.cheffy.domain.fooditem.port.output.FoodItemRepository;
 import br.com.fiap.cheffy.domain.restaurant.entity.Restaurant;
 import br.com.fiap.cheffy.domain.restaurant.exception.RestaurantNotFoundException;
@@ -34,6 +35,16 @@ public class FoodItemServiceHelper {
     public FoodItem getFoodItemOrFail(UUID foodItemId, UUID restaurantId) {
         return foodItemRepository.findByIdAndRestaurantId(foodItemId, restaurantId)
                 .orElseThrow(() -> new FoodItemNotFoundException(FOOD_ITEM_NOT_FOUND, foodItemId));
+    }
+
+    public FoodItem getAvailableFoodItemOrFail(UUID foodItemId, UUID restaurantId) {
+        FoodItem foodItem = getFoodItemOrFail(foodItemId, restaurantId);
+
+        if (!foodItem.isActive() || !foodItem.isAvailable()) {
+            throw new FoodItemUnavailableForOrderException(FOOD_ITEM_UNAVAILABLE_FOR_ORDER, foodItemId);
+        }
+
+        return foodItem;
     }
 
     public void saveFoodItem(FoodItem foodItem) {
