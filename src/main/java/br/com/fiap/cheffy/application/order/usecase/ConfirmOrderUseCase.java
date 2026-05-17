24cbd7074bb96eb12d69a7f1b78a13b1809f85fe
thiagosslima <thiagosslima@MacBook-Pro-M4-Thiago-Soares.local>
2026-05-16 22:37:12 -0300

    refact: Limpeza de importações

--- src/main/java/br/com/fiap/cheffy/application/order/usecase/ConfirmOrderUseCase.java
+++ src/main/java/br/com/fiap/cheffy/application/order/usecase/ConfirmOrderUseCase.java
@@ -11,7 +11,6 @@ import br.com.fiap.cheffy.domain.order.port.output.OrderRepository;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.UUID;
-import java.util.logging.Level;
 
 import static br.com.fiap.cheffy.shared.exception.keys.ExceptionsKeys.ORDER_NOT_FOUND_EXCEPTION;
 
